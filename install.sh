aab88dc23244a146fcd9c22e3f314fbc80bd3082
SUFandom <70519093+SUFandom@users.noreply.github.com>
2022-09-17 21:21:56 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,28 @@ clear
 echo "Loading"
 apt-get update >> /dev/null
 apt-get install sudo xfce4 xfce4-goodies tigervnc* firefox-esr -y | dialog --programbox 30 65
+echo "Preparing Additional Tools in Readline"
+chmod +x resources/identify
+cp resources/identify /usr/bin
+mkdir /usr/share/image/
+cp resources/icon.png /usr/share/image/
+cp resources/os-release /usr/lib/os-release
+mkdir /usr/lib/diagnose
+chmod +x resources/diagnose
+cp resources/diagnose /usr/bin
+cp resources/Visual-Studio-alt.desktop /usr/lib/diagnose
+cp resources/Visual-Studio.desktop /usr/lib/diagnose
+mkdir $HOME/.config
+cp -r resources/config/xfce4 $HOME/.config/
+cp resources/config/.bashrc $HOME/.config/
+mkdir /etc/skel/.config
+cp -r resources/config/xfce4 /etc/skel/.config
+cp resources/config/.bashrc /etc/skel/
+chmod +x resources/diagnose
+cp resources/diagnose /bin
+mkdir /usr/share/images/
+mkdir /usr/share/images/desktop-base
+tar -xfJ wp/destiny.tar.xz -C /usr/share/images/desktop-base
 if (whereis firefox-esr)
 then
 echo ""
@@ -38,9 +60,29 @@ apt-get install sudo xfce4 xfce4-goodies qtcreator glade tigervnc* firefox-esr -
 echo "Switching to Readline to download asset"
 wget https://github.com/SUFandom/debdroid-gitserver/releases/download/vs-code-1.70/code_1.70.1-1660111763_armhf.deb 
 mkdir $HOME/Desktop
-wget https://github.com/SUFandom/debdroid-gitserver/releases/download/vs-code-1.70/Visual.Studio.Code.desktop.txt
-mv Visual.Studio.Code.desktop.txt $HOME/Desktop/VSCode.desktop
 dpkg -i code_1.70.1-1660111763_armhf.deb | dialog --programbox 30 65
+chmod +x resources/identify
+cp resources/identify /usr/bin
+mkdir /usr/share/image/
+cp resources/icon.png /usr/share/image/
+cp resources/os-release /usr/lib/os-release
+mkdir /usr/lib/diagnose
+chmod +x resources/diagnose
+cp resources/diagnose /usr/bin
+cp resources/Visual-Studio-alt.desktop /usr/lib/diagnose
+cp resources/Visual-Studio.desktop /usr/lib/diagnose
+mkdir $HOME/.config
+cp -r resources/config/xfce4 $HOME/.config/
+cp resources/config/.bashrc $HOME/.config/
+mkdir /etc/skel/.config
+cp -r resources/config/xfce4 /etc/skel/.config
+cp resources/config/.bashrc /etc/skel/
+chmod +x resources/diagnose
+cp resources/diagnose /bin
+mkdir /usr/share/images/
+mkdir /usr/share/images/desktop-base
+tar -xfJ wp/destiny.tar.xz -C /usr/share/images/desktop-base
+cp resources/Visual-Studio.desktop $HOME/Desktop/VSCode.desktop
 if (whereis firefox-esr)
 then
 echo ""
